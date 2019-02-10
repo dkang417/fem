50c69dba695fffc1f54becd8cf099f5b2a9a6af5
dkang417 <dkang417@gmail.com>
2019-02-09 20:10:07 -0500

    update

---
+++ src/App.js
@@ -0,0 +1,18 @@
+const Pet = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "luna"),
+        React.createElement("h2", {}, "dog"),
+        React.createElement("h2", {}, "cat")
+    ]);
+}
+
+const App = () => {
+    return React.createElement("div", {},[
+        React.createElement('h1', {}, 'Adopt me!'),
+        React.createElement(Pet),
+        React.createElement(Pet),
+        React.createElement(Pet)
+    ])
+}     
+
+ReactDom.render(React.createElement(App), document.getElementById('root'))   
