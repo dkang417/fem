e408207ac9473ee55d65272c33b49e62ced6b8dd
dkang417 <dkang417@gmail.com>
2019-02-10 20:40:45 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-const Pet = () => {
+import React from 'react';
+import { render } from 'react-dom';
+
+const Pet = (props) => {
     return React.createElement("div", {}, [
-        React.createElement("h1", {}, "luna"),
-        React.createElement("h2", {}, "dog"),
-        React.createElement("h2", {}, "cat")
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed)
     ]);
 }
 
-const App = () => {
-    return React.createElement("div", {}, [
-        React.createElement('h1', {}, 'Adopt me!'),
-        React.createElement(Pet),
-        React.createElement(Pet),
-        React.createElement(Pet)
-    ])
-}     
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'))   
+
+class App extends React.Component {
+
+    render() {
+        return React.createElement("div", {}, [
+            React.createElement('h1', {}, 'Adopt me!'),
+            React.createElement(Pet, {
+                name: 'luna', animal: 'dog', breed: 'havanese'
+            }),
+            React.createElement(Pet, {
+                name: 'jack', animal: 'bird', breed: 'parrot'
+            }),
+            React.createElement(Pet, {
+                name: 'david', animal: 'cat', breed: 'mixed'
+            }),
+        ])
+    }
+}    
+
+render(React.createElement(App), document.getElementById('root'))   
