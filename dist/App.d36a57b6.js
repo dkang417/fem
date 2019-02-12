cd7dd9abd65d8d4c642043c4f91a8118bf2995e6
dkang417 <dkang417@gmail.com>
2019-02-12 13:43:01 -0500

    add carousel

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -28430,7 +28430,100 @@ function (_React$Component) {
 
 var _default = Results;
 exports.default = _default;
-},{"react":"../node_modules/react/index.js","./pet":"pet.js","petfinder-client":"../node_modules/petfinder-client/index.js"}],"Details.js":[function(require,module,exports) {
+},{"react":"../node_modules/react/index.js","./pet":"pet.js","petfinder-client":"../node_modules/petfinder-client/index.js"}],"Carosel.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+var Carosel =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(Carosel, _React$Component);
+
+  function Carosel(props) {
+    var _this;
+
+    _classCallCheck(this, Carosel);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(Carosel).call(this, props));
+    _this.state = {
+      photos: [],
+      active: 0
+    };
+    return _this;
+  }
+
+  _createClass(Carosel, [{
+    key: "render",
+    value: function render() {
+      var _this$state = this.state,
+          photos = _this$state.photos,
+          active = _this$state.active;
+      return _react.default.createElement("div", {
+        className: "carousel"
+      }, _react.default.createElement("img", {
+        src: photos[active].value,
+        alt: "primary animal"
+      }), _react.default.createElement("div", {
+        className: "carousel-smaller"
+      }, photos.map(function (photo, index) {
+        return _react.default.createElement("img", {
+          key: photo.value,
+          src: photo.value,
+          className: index === active ? 'active' : '',
+          alt: "animal thumbnail"
+        });
+      })));
+    }
+  }], [{
+    key: "getDerivedStateFromProps",
+    value: function getDerivedStateFromProps(_ref) {
+      var media = _ref.media;
+      var photos = [];
+
+      if (media && media.photos && media.photos.photo) {
+        photos = media.photos.photo.filter(function (photo) {
+          return photo['@size'] === 'pn';
+        });
+      }
+
+      return {
+        photos: photos
+      };
+    }
+  }]);
+
+  return Carosel;
+}(_react.default.Component);
+
+var _default = Carosel;
+exports.default = _default;
+},{"react":"../node_modules/react/index.js"}],"Details.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -28443,6 +28536,8 @@ var _react = _interopRequireDefault(require("react"));
 var _petfinderClient = _interopRequireDefault(require("petfinder-client"));
 
 var _router = require("@reach/router");
+
+var _Carosel = _interopRequireDefault(require("./Carosel"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -28532,8 +28627,13 @@ function (_React$Component) {
           animal = _this$state.animal,
           breed = _this$state.breed,
           location = _this$state.location,
-          description = _this$state.description;
-      return _react.default.createElement("div", null, _react.default.createElement("div", null, _react.default.createElement("h1", null, name), _react.default.createElement("h2", null, animal, " - ", breed, " - ", location, " "), _react.default.createElement("p", null, description)));
+          description = _this$state.description,
+          media = _this$state.media;
+      return _react.default.createElement("div", {
+        className: "details"
+      }, _react.default.createElement(_Carosel.default, {
+        media: media
+      }), _react.default.createElement("div", null, _react.default.createElement("h1", null, name), _react.default.createElement("h2", null, animal, " - ", breed, " - ", location, " "), _react.default.createElement("p", null, description)));
     }
   }]);
 
@@ -28542,7 +28642,7 @@ function (_React$Component) {
 
 var _default = Details;
 exports.default = _default;
-},{"react":"../node_modules/react/index.js","petfinder-client":"../node_modules/petfinder-client/index.js","@reach/router":"../node_modules/@reach/router/es/index.js"}],"App.js":[function(require,module,exports) {
+},{"react":"../node_modules/react/index.js","petfinder-client":"../node_modules/petfinder-client/index.js","@reach/router":"../node_modules/@reach/router/es/index.js","./Carosel":"Carosel.js"}],"App.js":[function(require,module,exports) {
 "use strict";
 
 var _react = _interopRequireDefault(require("react"));
@@ -28630,7 +28730,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59567" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50572" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import pf from 'petfinder-client';
 import { navigate } from '@reach/router';
+import Carosel from './Carosel';
 
 const petfinder = pf({
     key: process.env.API_KEY,
@@ -52,10 +53,11 @@ class Details extends React.Component {
             return <h1>loading....</h1>
         }
 
-        const { name, animal, breed, location, description } = this.state;
+        const { name, animal, breed, location, description, media } = this.state;
 
         return ( 
-            <div>
+            <div className="details">
+                <Carosel media={media} />    
                 <div>
                     <h1>{name}</h1>
                     <h2>{animal} - {breed} - {location} </h2>
