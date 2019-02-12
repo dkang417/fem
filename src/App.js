6bc7b1b6745c5186b4994baae98f26a6b1d6831c
dkang417 <dkang417@gmail.com>
2019-02-11 20:41:54 -0500

    details

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Results from './Results';
+import Details from './Details';
+import { Router, Link } from '@reach/router';
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Adopt me!!!!</h1>
-                <Results />
+                <header> 
+                    <Link to="/">  Adopt me </Link>
+                </header>  
+                <Router>
+                    <Results path="/"/>
+                    <Details path="/details/:id" />
+                </Router>
             </div>
         );
     }
